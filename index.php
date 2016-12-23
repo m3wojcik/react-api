b0c177c5d28504cfe571215fcb69c7f26b8d4f1b
Mariusz <mariusz.wojcik@langlion.com>
2016-12-23 15:56:31 +0100

    marks

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+sleep(1);
 switch ($_GET["q"]) {
     case "getTableData":
         echo '[
@@ -24,18 +25,17 @@ switch ($_GET["q"]) {
                "name": "Angielski",
                "group": "Angielski 3/2016",
                "room": "Manchester",
-               "date": "16.12.2016",
-               "startTime": "17:00",
-               "endTime": "17:45",
+               "date": "2016-12-12 08:00:00",
+               "length": 45,
                "weekday": "Friday",
                "teacher": "Lektorski Jan",
                "active" : false,
                "status" : {"name": "zajęcia odwołano", "color":"#d9534f"},
                "details": []
              },
-            {"id": 2, "name": "Francuski", "group": "Francuski 4/2016", "room": "Chelsea", "date": "18.12.2016", "startTime": "16:00", "endTime": "16:45", "weekday": "Monday", "teacher": "Pier Karde", "active" : true, "status":null, "details": [{"name":"uzupełniono","class":"label-success"},{"name":"dodano pliki","class":"label-info"}]},
-            {"id": 3, "name": "Angielski", "group": "Angielski 3/2016", "room": "Manchester", "date": "20.12.2016", "startTime": "17:00", "endTime": "17:45", "weekday": "Friday", "teacher": "Lektorski Jan", "active" : true, "status":null, "details": [{"name":"uzupełniono","class":"label-success"}]},
-            {"id": 4, "name": "Francuski", "group": "Francuski 4/2016", "room": "Chelsea", "date": "22.12.2016", "startTime": "16:00", "endTime": "16:45", "weekday": "Monday", "teacher": "Pier Karde", "active" : true, "status":null, "details": [{"name":"uzupełniono","class":"label-success"}]}
+            {"id": 2, "name": "Francuski", "group": "Francuski 4/2016", "room": "Chelsea", "date": "2016-12-23 18:00:00", "length": 45, "weekday": "Monday", "teacher": "Pier Karde", "active" : true, "status":null, "details": [{"name":"uzupełniono","class":"label-success"},{"name":"dodano pliki","class":"label-info"}]},
+            {"id": 3, "name": "Angielski", "group": "Angielski 3/2016", "room": "Manchester", "date": "2016-12-24 18:00:00", "length": 45, "weekday": "Friday", "teacher": "Lektorski Jan", "active" : true, "status":null, "details": [{"name":"uzupełniono","class":"label-success"}]},
+            {"id": 4, "name": "Francuski", "group": "Francuski 4/2016", "room": "Chelsea", "date": "2016-02-25 16:00:00", "length": 45, "weekday": "Monday", "teacher": "Pier Karde", "active" : true, "status":null, "details": [{"name":"uzupełniono","class":"label-success"}]}
           ]';
     break;
     case "getClassDetails":
@@ -52,5 +52,19 @@ switch ($_GET["q"]) {
                "status" : {"name": "zajęcia odwołano", "color":"#d9534f"},
                "details": [{"name":"uzupełniono","class":"label-success"},{"name":"dodano pliki","class":"label-info"}]
              }';
+    break;
+    case "getNotifications":
+        echo '[
+          {"id": 1, "type": "calendar", "name": "Wystąpiły zmiany w planie zajęć w grupie Angielski/Czw/Pt/2015.","date": "2016-12-23 18:00:00" },
+          {"id": 2, "type": "calendar", "name": "Wystąpiły zmiany w planie zajęć w grupie Angielski/Czw/Pt/2015.", "date": "2016-12-23 18:00:00" },
+          {"id": 3, "type": "calendar", "name": "Wystąpiły zmiany w planie zajęć w grupie Angielski 3/2015.", "date": "2016-12-23 18:00:00" },
+          {"id": 4, "type": "message", "name": "Wiadomość od Langlion System 27.08.2015 16:39.", "date": "2016-12-23 18:00:00" },
+          {"id": 5, "type": "calendar","name": "Wystąpiły zmiany w planie zajęć w grupie Angielski/Czw/Pt/2015.", "date": "2016-12-23 18:00:00" },
+        ]';
+    break;
+    case "getMarks":
+        echo '[
+          {"groupId": 1, "groupName": "Angielski 3/2016", "marks": [{ "name": "test", "value": "60/100", "date": "2016-12-23 18:00:00" }, { "name": "Speaking", "value": "80/100", "date": "2016-12-20 18:00:00" }], "percent": 73 }
+        ]';
     break;
 }
